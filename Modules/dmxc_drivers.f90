362d4e3e2146b00bbf8cdb04b7737d4a7fd6ae5c
Carlo Cavazzoni <c.cavazzoni@cineca.it>
2020-04-13 14:39:21 +0200

    merged

--- Modules/dmxc_drivers.f90
+++ Modules/dmxc_drivers.f90
@@ -219,8 +219,8 @@ SUBROUTINE dmxc_lda( length, rho_in, dmuxc )
   !
   IF (iexch == 1 .AND. icorr == 1) THEN
   !
-!$omp parallel if(ntids==1)
-!$omp do private( rs, rho, ex_s, vx_s , iflg)
+!!$omp parallel if(ntids==1)
+!!$omp do private( rs, rho, ex_s, vx_s , iflg)
      DO ir = 1, length
         !
         rho = rho_in(ir)
@@ -242,8 +242,8 @@ SUBROUTINE dmxc_lda( length, rho_in, dmuxc )
         dmuxc(ir) = dmuxc(ir) * SIGN(1.0_DP,rho_in(ir))
         !
      ENDDO
-!$omp end do
-!$omp end parallel
+!!$omp end do
+!!$omp end parallel
      !
   ELSE
      !
