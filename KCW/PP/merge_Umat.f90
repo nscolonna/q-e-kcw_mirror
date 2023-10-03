0dcb03d00801ce2f59d23033f9c6dd926e466941
ncolonna <nicola.colonna@psi.ch>
2023-10-03 10:37:24 +0200

    improved structure and output log of merge_Umat.x

--- KCW/PP/merge_Umat.f90
+++ KCW/PP/merge_Umat.f90
@@ -2,7 +2,7 @@
 
 PROGRAM merge
  ! Utility program to merge different U matrices in a single U
- ! Usage: merge_Umat.x <path/to/Umat1> <path/to/Umat2> ... <path/to/UmatN>
+ ! Usage: merge_Umat.x seedname <path/to/Umat1> <path/to/Umat2> ... <path/to/UmatN>
  !
  IMPLICIT NONE
  integer :: nk1, nk2, nb1, nb2
@@ -13,44 +13,51 @@ PROGRAM merge
  real*16, allocatable :: k(:,:)
  real*16 :: re, im
  integer :: ik, ib, jb, ii, iib, jjb
- character(256) :: dum,arg
+ character(256) :: dum,arg, seedname
  character(256), allocatable :: filename(:)
  integer :: nargs, iarg, ni, nf, nb1_
  !
  nargs = command_argument_count()
- write(*,*) nargs 
+ !write(*,*) nargs 
  IF (nargs == 0 ) STOP
  allocate (filename(nargs) )
+ WRITE(*,*) "Merging ",nargs-1, " blocks" !, seedname
  !
  nb=0
- iarg = 0
+ iarg = 1
+ CALL get_command_argument( iarg, arg )
+ seedname=trim(arg)
+ WRITE(*,*) "Seedname = ",  trim(seedname)
+ !
  DO WHILE ( iarg < nargs )
    iarg=iarg +1
    CALL get_command_argument( iarg, arg )
-   filename(iarg)=trim(arg)
-   WRITE(*,*) filename(iarg)
+   !write (*,*) trim(arg), seedname, 'u_mat'
+   filename(iarg)=trim(arg)//'/'//trim(seedname)//trim('_u.mat')
    open (27, file=filename(iarg))
    read(27,*) dum
    read(27,*) nk1, nb1, nb1
    close(27)
+   WRITE(*,*) "block # ", iarg-1, "file = ", trim(filename(iarg)), " nbnd = ", nb1
    nb = nb + nb1
  ENDDO
  !
- write(*,*) nb
+ write(*,*) "Final # of bands   = ", nb
+ write(*,*) "Final # of kpoints = ", nk1
  ALLOCATE( U(nb,nb,nk1))
  ALLOCATE (k(3,nk1))
  U=cmplx(0.D0,0.D0)
  !
  nb1_=0
- do iarg = 1, nargs
+ do iarg = 2, nargs
    open (27, file=filename(iarg))
-   WRITE(*,*) filename(iarg)
+   WRITE(*,*) "Reading ", TRIM(filename(iarg))
    read(27,*) dum
    read(27,*) nk1, nb1, nb1
-   write(*, '(3I12)') nk1, nb1, nb1
+   !write(*, '(3I12)') nk1, nb1, nb1
    ni=1+nb1_
    nf=nb1+nb1_
-   write(*,*) ni, nf, nb1_
+   write(*,*) "istart = ", ni, " iend = ", nf !, " increment =", nb1_
    do ik = 1, nk1
       read (27, *)
       !write(*,*) 
@@ -66,8 +73,10 @@ PROGRAM merge
    !
  end do
  !
- write(*,*) nb, nk1
- WRITE(*,'(2(f15.10,sp,f15.10))') U(1,1,1), U(nb,nb,nk1)
+ write(*,*) "Final # of bands   = ", nb
+ write(*,*) "Final # of kpoints = ", nk1
+ !WRITE(*,'(2(f15.10,sp,f15.10))') U(1,1,1), U(nb,nb,nk1)
+ WRITE(*,*) "Writing ", TRIM('wann_u.mat')
  open (40, file='wann_u.mat', form='formatted')
  write (40, *) "Written by merge.x" 
  write (40, '(3I12)') nk1, nb, nb
@@ -78,6 +87,7 @@ PROGRAM merge
    !write(40, *) ((U(ib, jb, ik), ib=1, nb), jb=1, nb)
    !write(*, *) ((U(ib, jb, ik), ib=1, nb), jb=1, nb)
  enddo
+ WRite (*,'(/, " GAME OVER", /)') 
  !
  !
 END program
