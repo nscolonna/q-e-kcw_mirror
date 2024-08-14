8d25cf219b072bcd41faea173d4da535e717817b
gcistaro <giovannicistaro@gmail.com>
2024-08-14 11:06:19 +0200

    Deallocating global arrays related to symmetry

--- KCW/src/kcw_deallocate_q.f90
+++ KCW/src/kcw_deallocate_q.f90
@@ -61,6 +61,19 @@ SUBROUTINE kcw_deallocate_q()
   if (allocated(vsgga))           deallocate (vsgga)
   if (allocated(gmag))            deallocate (gmag)
   !
+  ! Symmetry related arrays
+  if( allocated(rir)) deallocate(rir)
+  if( allocated(r)) deallocate(r)
+  if( allocated(s_w)) deallocate(s_w)
+  if( allocated(ft_w)) deallocate(ft_w)
+  if( allocated(nsym_w)) deallocate(nsym_w)
+  if( allocated(nqstot_ibz)) deallocate(nqstot_ibz)
+  if( allocated(xq_ibz)) deallocate(xq_ibz)
+  if( allocated(wq_ibz)) deallocate(wq_ibz)
+  if( allocated(ibz2fbz)) deallocate(ibz2fbz)
+  if( allocated(fbz2ibz)) deallocate(fbz2ibz)
+  !
+  !
   RETURN
   !
 END SUBROUTINE kcw_deallocate_q
